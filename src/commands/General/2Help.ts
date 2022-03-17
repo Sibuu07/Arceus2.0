b11b710f22578ab57172fb5c83cd29b92d8720ca
NemasisDarkX <68656546+NemasisDarkX@users.noreply.github.com>
2022-03-17 20:59:47 +0530

    Update 2Help.ts

--- src/commands/General/2Help.ts
+++ src/commands/General/2Help.ts
@@ -36,9 +36,8 @@ export default class Command extends BaseCommand {
                 }
             }
             let text = `
-╭─「(づ￣ 3￣)づ」
 │⋊ ᴜꜱᴇʀ: *${M.sender.username}*
-│⋊ ɴᴀᴍᴇ: ᖇᎥᑎ
+│⋊ ɴᴀᴍᴇ: ITACHI UCHIHA
 │⋊ ᴘʀᴇꜰɪx: ${this.client.config.prefix}
 │⋊ ᴏᴡɴᴇʀ: <${this.client.config.prefix}mod>
 ╰────────────┈平和                            \n\n`
@@ -52,12 +51,11 @@ export default class Command extends BaseCommand {
             return void this.client.sendMessage(M.from, { url: rin }, MessageType.video, {quoted:M.WAMessage,
             mimetype: Mimetype.gif,
             caption: `${text}
- ──❅┈[ ᖇᎥᑎ ᗷᗝ丅 ]┈❅───
+ ──❅┈[ UCHIHA BOTS ]┈❅───
 ┌────────────┈❅
-│   🧨 ᖇᎥᑎ
+│   🧨 ITACHI
 │   ©️ Synthesized Infinity Botto
 └────────────┈⁂
-❅┈[𝐇𝐚𝐯𝐞 𝐆𝐫𝐞𝐚𝐭 𝐃𝐚𝐲]┈❅
 🎗 *Note: Use ${this.client.config.prefix}help <command_name> to view the command info*` }
             )
         }
