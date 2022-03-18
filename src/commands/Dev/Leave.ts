7f9334a4187ad16ef770a18926ccbbc2809e09d1
NemasisDarkX <68656546+NemasisDarkX@users.noreply.github.com>
2022-03-18 10:08:38 +0530

    Update Leave.ts

--- src/commands/Dev/Leave.ts
+++ src/commands/Dev/Leave.ts
@@ -1,6 +1,7 @@
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
+import { join } from 'path
 import { ISimplifiedMessage } from '../../typings'
 
 export default class Command extends BaseCommand {
@@ -19,13 +20,14 @@ export default class Command extends BaseCommand {
     run = async (M: ISimplifiedMessage): Promise<void> => {
         await  run = async (M: ISimplifiedMessage): Promise<void> => {
         const n = [
-            './assets/ITACHI/middle-finger.mp4'
+            './assets/ITACHI/leave.mp4'
         ]
         let rin = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: rin }, MessageType.video, {quoted:M.WAMessage,
             mimetype: Mimetype.gif
                                                                                      }
                                             )
+        }
         await this.client.groupLeave(M.from).catch(() => M.reply('Failed to leave the Group'))
     }
 }
