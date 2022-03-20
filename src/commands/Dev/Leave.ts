b6145766c6439261c880a93f8eb14595e2fef1dd
NemasisDarkX <68656546+NemasisDarkX@users.noreply.github.com>
2022-03-20 11:23:46 +0530

    Update Leave.ts

--- src/commands/Dev/Leave.ts
+++ src/commands/Dev/Leave.ts
@@ -1,7 +1,5 @@
-import { MessageType } from '@adiwajshing/baileys/lib/WAConnection'
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
-import request from '../../lib/request'
 import WAClient from '../../lib/WAClient'
 import { ISimplifiedMessage } from '../../typings'
 
@@ -19,14 +17,9 @@ export default class Command extends BaseCommand {
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
-    await M.reply( await request.buffer(`https://safebooru.org/samples/3069/sample_10a3b5dc359a1367eabca4a18ccb1235f2de51fd.jpg?3194721`),
-        MessageType.image,
-                    undefined,
-                    undefined,
-                    `*Sayonara Mina* 👋\n`,
-                    undefined
-                    ).catch((reason: any) =>
-                    M.reply(`✖ An error occurred. Please try again later.`))
+        await M.reply( await request.buffer(`https://i.ibb.co/3d7hDSZ/thumb-1920-1114169.jpg`),
+        MessageType.vedio,
+        await M.reply(`*bye bye* 👋`)
         await this.client.groupLeave(M.from).catch(() => M.reply('Failed to leave the Group'))
     }
 }
